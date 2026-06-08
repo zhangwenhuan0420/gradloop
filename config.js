44685bc223c542e4c8fe1396be79b76ef22f6630
zhangwenhuan0420 <zhangwenhuan0420@gmail.com>
2026-06-08 02:04:01 +0100

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GRADLOOP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://rtlebdivzzmqnushmaeo.supabase.co",
+  supabaseAnonKey: "sb_publishable_i4eD2vKugjgVtVQ3tVC9Wg_5laUlg3s",
   adminEmails: ["zhangwenhuan0420@gmail.com"],
 };
